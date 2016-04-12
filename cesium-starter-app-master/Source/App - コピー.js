188e46e31866a8aab684124ce0458826c378a0c6
hikasan <wo__48@hotmail.com>
2016-04-12 16:47:08 +0900

    debug

--- cesium-starter-app-master/Source/App - コピー.js
+++ cesium-starter-app-master/Source/App - コピー.js
@@ -1,1 +1,42 @@
-var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+/*
+var viewer = new Cesium.Viewer('cesiumContainer', {
+  imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+    url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
+      credit: new Cesium.Credit('ínóùâ@É^ÉCÉã', '', 'http://maps.gsi.go.jp/development/ichiran.html')
+    }),
+  baseLayerPicker: false
+}); 
+viewer.dataSources.add(Cesium.KmlDataSource.load('file://D:/work\cesium-starter-app-master/Source/test.kml'))
+
+//add point
+var point = viewer.entities;
+point.add({
+  position : Cesium.Cartesian3.fromDegress(38.938605,141.1048496),
+  point : {
+    pixelSize : 1000,
+    color : Cesium.Color.YELLOW
+  }
+});
+
+//add a line
+var line1 = viewer.entities;
+line1.add({
+  name : 'Red line on the surface',
+  polyline : {
+    positions : Cesium.Cartesian3.fromDegreesArray([-75, 35, -125, 35]),
+    width : 500,
+    material : Cesium.Color.RED
+  }
+});
+//add polygon
+var Polygon = viewer.entities;
+Polygon.add({
+  name : 'Polygon',
+  polygon : {
+    hierarchy : Cesium.Cartesian3.fromDegreesArray([-108.0, 42.0, -100.0, 42.0, -104.0, 40.0]),
+    extrudedHeight: 500000.0,
+    material : Cesium.Color.GREEN
+  }
+});
+*/
+
