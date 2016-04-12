a0dcda23f64ce96a51bdaabd5f5ff4b48cd4ed03
hikasan <wo__48@hotmail.com>
2016-04-12 15:29:56 +0900

    kml-format

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -39,10 +39,10 @@ Polygon.add({
   }
 });
 */
-var geodata = 'http://hikasan.github.io/CesiumTEST/cesium-starter-app-master/Source/test.geojson';
-
-var promise = Cesium.GeoJsonDataSource.load(geodata);
-
+//var geodata = 'http://hikasan.github.io/CesiumTEST/cesium-starter-app-master/Source/test.geojson';
+//var promise = Cesium.GeoJsonDataSource.load(geodata);
+var kmldata = 'http://hikasan.github.io/CesiumTEST/cesium-starter-app-master/Source/test.kml'
+var promise = Cesium.KmlDataSource.load(geodata);
 
 promise.then(function(datasource){
   var viewer = new Cesium.Viewer('mapdiv', {
