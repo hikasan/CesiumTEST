36bd3b6b3a16a7a25aaeee6bcf3e07fad2e966bb
hikasan <wo__48@hotmail.com>
2016-05-07 14:20:08 +0900

    debug

---
+++ cesium-starter-app-master/Source/App - コピー (4).js
@@ -0,0 +1,124 @@
+﻿var viewer = new Cesium.Viewer('mapdiv', {
+    animation : false,
+    baseLayerPicker: false,
+    fullscreenButton: false,
+    geocoder: false,
+    homeButton: false,
+    navigationHelpButton: false,
+    sceneModePicker: false,
+    scene3DOnly: true,
+    timeline: false,
+    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+      url: '//cyberjapandata.gsi.go.jp/xyz/relief/'
+    }),
+    terrainProvider: new Cesium.JapanGSITerrainProvider({
+      heightPower: 1.0
+    });
+
+/*
+addDefaultToolbarButton('Default styling', function() {
+    viewer.dataSources.add(Cesium.GeoJsonDataSource.load('/Source/test.geojson'));
+});
+
+addToolbarButton('Basic styling', function() {
+    viewer.dataSources.add(Cesium.GeoJsonDataSource.load('/Source/test.geojson', {
+        stroke: Cesium.Color.HOTPINK,
+        fill: Cesium.Color.PINK.withAlpha(0.5),
+        strokeWidth: 3
+    }));
+});
+*/
+
+//addToolbarButton('Custom styling', function() {
+    //Seed the random number generator for repeatable results.
+/*
+    Cesium.Math.setRandomNumberSeed(0);
+
+    var promise = Cesium.GeoJsonDataSource.load('/Source/test.geojson');
+    promise.then(function(dataSource) {
+        viewer.dataSources.add(dataSource);
+
+        //Get the array of entities
+        var entities = dataSource.entities.values;
+        
+        var colorHash = {};
+        for (var i = 0; i < entities.length; i++) {
+            //For each entity, create a random color based on the state name.
+            //Some states have multiple entities, so we store the color in a
+            //hash so that we use the same color for the entire state.
+            var entity = entities[i];
+            var name = entity.name;
+            var color = colorHash[name];
+            if (!color) {
+                color = Cesium.Color.fromRandom({
+                    alpha : 1.0
+                });
+                colorHash[name] = color;
+            }
+            
+            //Set the polygon material to our random color.
+            entity.polygon.material = color;
+            //Remove the outlines.
+            entity.polygon.outline = false;
+
+            //Extrude the polygon based on the state's population.  Each entity
+            //stores the properties for the GeoJSON feature it was created from
+            //Since the population is a huge number, we divide by 50.
+            entity.polygon.extrudedHeight = entity.properties.AveragevValue * 100000.0;
+        }
+    }).otherwise(function(error){
+        //Display any errrors encountered while loading.
+        window.alert(error);
+    });
+*/
+//});
+
+
+/*
+Sandcastle.reset = function() {
+  viewer.dataSources.removeAll();
+  
+  viewer.camera.lookAt(Cesium.Cartesian3.fromDegrees(-98.0, 40.0), new Cesium.Cartesian3(0.0, -4790000.0, 3930000.0));
+  viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
+};
+*/
+
+/*
+var geodata = 'http://hikasan.github.io/CesiumTEST/cesium-starter-app-master/Source/test.kml';
+var promise = Cesium.KmlDataSource.load(geodata);
+*/
+
+/*
+var geodata = '/Source/test.geojson';
+var promise = Cesium.GeoJsonDataSource.load(geodata);
+
+
+promise.then(function(datasource){
+  var viewer = new Cesium.Viewer('mapdiv', {
+    animation : false,
+    baseLayerPicker: false,
+    fullscreenButton: false,
+    geocoder: false,
+    homeButton: false,
+    navigationHelpButton: false,
+    sceneModePicker: false,
+    scene3DOnly: true,
+    timeline: false,
+    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+      url: '//cyberjapandata.gsi.go.jp/xyz/relief/'
+    }),
+    terrainProvider: new Cesium.JapanGSITerrainProvider({
+      heightPower: 1.0
+    })
+  });
+
+  var layers = viewer.scene.imageryLayers;
+  var osm = layers.addImageryProvider(
+    new Cesium.OpenStreetMapImageryProvider()
+  );
+  osm.alpha = 0.6;
+
+  viewer.dataSources.add(datasource);
+  viewer.zoomTo(datasource);
+});
+*/
