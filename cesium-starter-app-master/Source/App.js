e7a87bee5b3ac71c94b5eda8ae96fd07a9f7f054
hikasan <wo__48@hotmail.com>
2016-05-07 14:25:18 +0900

    debug

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -5,3 +5,32 @@
     }),
     baseLayerPicker: false
   });
+
+Cesium.Math.setRandomNumberSeed(0);
+
+var promise = Cesium.GeoJsonDataSource.load('/Source/test.geojson');
+promise.then(function(dataSource) {
+    cesiumWidget.dataSources.add(dataSource);
+
+    var entities = dataSource.entities.values;
+    
+    var colorHash = {};
+    for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        var name = entity.name;
+        var color = colorHash[name];
+        if (!color) {
+            color = Cesium.Color.fromRandom({
+                alpha : 1.0
+            });
+            colorHash[name] = color;
+        }
+        
+        entity.polygon.material = color;
+        entity.polygon.outline = false;
+
+        entity.polygon.extrudedHeight = entity.properties.AveragevValue * 100000.0;
+    }
+}).otherwise(function(error){
+    window.alert(error);
+});
