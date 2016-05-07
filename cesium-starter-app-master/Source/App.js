7b47b75d464d42797e8df08c6c73df0093f8f203
hikasan <wo__48@hotmail.com>
2016-05-07 16:43:10 +0900

    debug

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -26,12 +26,12 @@ promise.then(function(dataSource) {
             colorHash[name] = color;
         }
         
-        //if (entity.geometry.type == "Polygon" ) {
+        if (entity.geometry.type == "Polygon" ) {
 	        entity.polygon.material = color;
 	        entity.polygon.outline = false;
 
 	        entity.polygon.extrudedHeight = entity.properties.AverageValue * 10000.0;
-        //}
+        }
     }
 }).otherwise(function(error){
     window.alert(error);
