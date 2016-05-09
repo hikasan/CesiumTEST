98289a2f6829173e7abd5c9eb91235c4ce8c2fd0
hikasan <wo__48@hotmail.com>
2016-05-09 15:42:17 +0900

    debug

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,20 +1,20 @@
 ﻿	var camera_start = Cesium.Cartesian3.fromDegrees(139.45, 35.41, 1000);
 	var camera_direction = Cesium.Cartesian3.fromDegrees(-75.0, 70.0, 0);
-	var hakone = './Data/test.geojson';
+	var testdata = './Data/test.geojson';
 
-	var promise = Cesium.GeoJsonDataSource.load(hakone);
+	var promise = Cesium.GeoJsonDataSource.load(testdata);
 
 	promise.then(function(datasource){
 	  var viewer = new Cesium.Viewer('mapdiv', {
-	    animation : false,
-	    baseLayerPicker: false,
-	    fullscreenButton: false,
-	    geocoder: false,
-	    homeButton: false,
-	    navigationHelpButton: false,
-	    sceneModePicker: false,
-	    scene3DOnly: true,
-	    timeline: false,
+//	    animation : false,
+//	    baseLayerPicker: false,
+//	    fullscreenButton: false,
+//	    geocoder: false,
+//	    homeButton: false,
+//	    navigationHelpButton: false,
+//	    sceneModePicker: false,
+//	    scene3DOnly: true,
+//	    timeline: false,
 	    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
 	      url: 'http://cyberjapandata.gsi.go.jp/xyz/relief/'
 	    }),
