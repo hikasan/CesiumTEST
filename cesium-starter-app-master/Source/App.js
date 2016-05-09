6b37fa220fd533310e245d38187172baf5097b84
hikasan <wo__48@hotmail.com>
2016-05-09 15:31:46 +0900

    debug

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,57 +1,5 @@
-﻿CesiumTEST.addDefaultToolbarButton('Default styling', function() {
-	var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
-	    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-	      url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
-	      credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
-	    }),
-	    baseLayerPicker: false
-	  });
-
-	Cesium.Math.setRandomNumberSeed(0);
-
-	var promise = Cesium.GeoJsonDataSource.load('./Data/test.geojson');
-	promise.then(function(dataSource) {
-	    cesiumWidget.dataSources.add(dataSource);
-
-	    var entities = dataSource.entities.values;
-	    
-	    var colorHash = {};
-	    for (var i = 0; i < entities.length; i++) {
-	        var entity = entities[i];
-	        var name = entity.name;
-	        var color = colorHash[name];
-	        if (!color) {
-	            color = Cesium.Color.fromRandom({
-	                alpha : 1.0
-	            });
-	            colorHash[name] = color;
-	        }
-	        if (entity.polygon != null) {
-		        entity.polygon.material = color;
-		        entity.polygon.outline = false;
-		        entity.polygon.extrudedHeight = entity.properties.AverageValue * 10000.0;
-	        }
-	        if (entity.corridor != null) {
-		        entity.corridor.outlineColor = color;
-		        //entity.corridor.extrudedHeight = entity.properties.AverageValue * 10000.0;
-	        }
-	        if (entity.point != null) {
-		        //entity.point.Color = color;
-		        //entity.billboard.image = './Images/Iplus.png';
-		        entity.point.pixelSize = 10;
-	            entity.point.color = Cesium.Color.YELLOW;
-
-		        //entity.billboard.scale = 2.0;
-	        }
-	    }
-	}).otherwise(function(error){
-	    window.alert(error);
-	});
-});
-
-CesiumTEST.addToolbarButton('Custom styling', function() {
-	var camera_start = Cesium.Cartesian3.fromDegrees(139.45, 35.41, 1000);
-	var camera_direction = Cesium.Cartesian3.fromDegrees(-75.0, 70.0, 0);
+﻿	//var camera_start = Cesium.Cartesian3.fromDegrees(139.45, 35.41, 1000);
+	//var camera_direction = Cesium.Cartesian3.fromDegrees(-75.0, 70.0, 0);
 	var hakone = './Data/test.geojson';
 
 	var promise = Cesium.GeoJsonDataSource.load(hakone);
@@ -97,7 +45,6 @@ CesiumTEST.addToolbarButton('Custom styling', function() {
 	});
 	var scene = cesiumWidget.scene;
 	scene.globe.depthTestAgainstTerrain = true;
-	scene.camera.lookAt(camera_start, camera_direction, Cesium.Cartesian3.UNIT_Z);
-});
+	//scene.camera.lookAt(camera_start, camera_direction, Cesium.Cartesian3.UNIT_Z);
 
 
