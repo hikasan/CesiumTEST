0fe3eb48972f0d350a5c4200a97b61b316e3c518
hikasan <wo__48@hotmail.com>
2016-05-09 11:52:21 +0900

    debug

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -35,9 +35,9 @@ promise.then(function(dataSource) {
 	        //entity.corridor.extrudedHeight = entity.properties.AverageValue * 10000.0;
         }
         if (entity.point != null) {
-	        entity.point.Color = color;
-	        entity.billboard.image = './Images/Iplus.gif';
-	        entity.billboard.scale = 2.0;
+	        //entity.point.Color = color;
+	        entity.billboard.image = './Images/Iplus.png';
+	        //entity.billboard.scale = 2.0;
         }
     }
 }).otherwise(function(error){
