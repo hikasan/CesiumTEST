9899af4d09380401dc33e894e5f6595bb0fca8a7
hikasan <wo__48@hotmail.com>
2016-05-09 15:45:12 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,50 +1,49 @@
-﻿	var camera_start = Cesium.Cartesian3.fromDegrees(139.45, 35.41, 1000);
-	var camera_direction = Cesium.Cartesian3.fromDegrees(-75.0, 70.0, 0);
-	var testdata = './Data/test.geojson';
+﻿
+var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+      url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
+      credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
+    }),
+    baseLayerPicker: false
+  });
 
-	var promise = Cesium.GeoJsonDataSource.load(testdata);
+Cesium.Math.setRandomNumberSeed(0);
 
-	promise.then(function(datasource){
-	  var viewer = new Cesium.Viewer('mapdiv', {
-//	    animation : false,
-//	    baseLayerPicker: false,
-//	    fullscreenButton: false,
-//	    geocoder: false,
-//	    homeButton: false,
-//	    navigationHelpButton: false,
-//	    sceneModePicker: false,
-//	    scene3DOnly: true,
-//	    timeline: false,
-	    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-	      url: 'http://cyberjapandata.gsi.go.jp/xyz/relief/'
-	    }),
-	    terrainProvider: new Cesium.JapanGSITerrainProvider({
-	      heightPower: 1.0
-	    })
-	  });
+var promise = Cesium.GeoJsonDataSource.load('./Data/test.geojson');
+promise.then(function(dataSource) {
+    cesiumWidget.dataSources.add(dataSource);
 
-	  var layers = viewer.scene.imageryLayers;
-	  var osm = layers.addImageryProvider(
-	    new Cesium.OpenStreetMapImageryProvider()
-	  );
-	  osm.alpha = 0.6;
+    var entities = dataSource.entities.values;
+    
+    var colorHash = {};
+    for (var i = 0; i < entities.length; i++) {
+        var entity = entities[i];
+        var name = entity.name;
+        var color = colorHash[name];
+        if (!color) {
+            color = Cesium.Color.fromRandom({
+                alpha : 1.0
+            });
+            colorHash[name] = color;
+        }
+        if (entity.polygon != null) {
+	        entity.polygon.material = color;
+	        entity.polygon.outline = false;
+	        entity.polygon.extrudedHeight = entity.properties.AverageValue * 10000.0;
+        }
+        if (entity.corridor != null) {
+	        entity.corridor.outlineColor = color;
+	        //entity.corridor.extrudedHeight = entity.properties.AverageValue * 10000.0;
+        }
+        if (entity.point != null) {
+	        //entity.point.Color = color;
+	        //entity.billboard.image = './Images/Iplus.png';
+	        entity.point.pixelSize = 10;
+            entity.point.color = Cesium.Color.YELLOW;
 
-	  viewer.dataSources.add(datasource);
-	  viewer.zoomTo(datasource);
-	});
-
-
-	var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
-	    imageryProvider: new Cesium.JapanGSIImageryProvider({
-	        layerLists: ["ort","relief","std"]
-	    }),
-	    terrainProvider: new Cesium.JapanGSITerrainProvider({
-	    }),
-	    baseLayerPicker: false,
-	    mapProjection: new Cesium.WebMercatorProjection(Cesium.Ellipsoid.WGS84)
-	});
-	var scene = cesiumWidget.scene;
-	//scene.globe.depthTestAgainstTerrain = true;
-	//scene.camera.lookAt(camera_start, camera_direction, Cesium.Cartesian3.UNIT_Z);
-
-
+	        //entity.billboard.scale = 2.0;
+        }
+    }
+}).otherwise(function(error){
+    window.alert(error);
+});
