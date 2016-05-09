0d713c23db79baa5bfaf1ac0b9318fe150fdf662
hikasan <wo__48@hotmail.com>
2016-05-09 13:13:05 +0900

    debug

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,4 +1,5 @@
-﻿var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
+﻿/*
+var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
     imageryProvider: new Cesium.OpenStreetMapImageryProvider({
       url: 'http://cyberjapandata.gsi.go.jp/xyz/std/',
       credit: new Cesium.Credit('地理院タイル', '', 'http://maps.gsi.go.jp/development/ichiran.html')
@@ -45,5 +46,38 @@ promise.then(function(dataSource) {
     }
 }).otherwise(function(error){
     window.alert(error);
+});
+*/
+var hakone = './Data/test.geojson';
+
+var promise = Cesium.GeoJsonDataSource.load(hakone);
+
+promise.then(function(datasource){
+  var viewer = new Cesium.Viewer('mapdiv', {
+    animation : false,
+    baseLayerPicker: false,
+    fullscreenButton: false,
+    geocoder: false,
+    homeButton: false,
+    navigationHelpButton: false,
+    sceneModePicker: false,
+    scene3DOnly: true,
+    timeline: false,
+    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+      url: '//cyberjapandata.gsi.go.jp/xyz/relief/'
+    }),
+    terrainProvider: new Cesium.JapanGSITerrainProvider({
+      heightPower: 1.0
+    })
+  });
+
+  var layers = viewer.scene.imageryLayers;
+  var osm = layers.addImageryProvider(
+    new Cesium.OpenStreetMapImageryProvider()
+  );
+  osm.alpha = 0.6;
+
+  viewer.dataSources.add(datasource);
+  viewer.zoomTo(datasource);
 });
 
