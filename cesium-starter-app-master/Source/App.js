0fbf003a34b45fb511ef941b4782b95dd72c19dc
hikasan <wo__48@hotmail.com>
2016-05-10 15:22:11 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -105,13 +105,14 @@
       baseLayerPicker: false,
       mapProjection: new Cesium.WebMercatorProjection(Cesium.Ellipsoid.WGS84)
   });
+/*
   var testdata = './Data/test.geojson';
   var promise = Cesium.GeoJsonDataSource.load(testdata);
   promise.then(function(datasource){
     cesiumWidget.dataSources.add(datasource);
     cesiumWidget.zoomTo(datasource);
   });
-
+*/
 
   var scene = cesiumWidget.scene;
   //scene.globe.depthTestAgainstTerrain = true;
