241b2d89389d69d36e902ab52a8c4070bf7fb70e
hikasan <wo__48@hotmail.com>
2016-05-10 17:12:15 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,34 +1,9 @@
-﻿
-var hakone = './Data/hakone_shelter.geojson';
-
-var promise = Cesium.GeoJsonDataSource.load(hakone);
-
-var cesiumWidget = new Cesium.Viewer('mapdiv', {
-  animation : false,
-  baseLayerPicker: false,
-  fullscreenButton: false,
-  geocoder: false,
-  homeButton: false,
-  navigationHelpButton: false,
-  sceneModePicker: false,
-  scene3DOnly: true,
-  timeline: false,
-  imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-    url: 'http://cyberjapandata.gsi.go.jp/xyz/relief/'
-  }),
-  terrainProvider: new Cesium.JapanGSITerrainProvider({
-    heightPower: 1.0
-  })
-});
-
-promise.then(function(datasource){
-  var layers = cesiumWidget.scene.imageryLayers;
-  var osm = layers.addImageryProvider(
-    new Cesium.OpenStreetMapImageryProvider()
-  );
-  osm.alpha = 0.6;
-
-  cesiumWidget.dataSources.add(datasource);
-  cesiumWidget.zoomTo(datasource);
-});
-
+﻿	var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
+	    imageryProvider: new Cesium.JapanGSIImageryProvider({
+	        layerLists: ["ort","relief","std"]
+	    }),
+	    terrainProvider: new Cesium.JapanGSITerrainProvider({
+	    }),
+	    baseLayerPicker: false,
+	    mapProjection: new Cesium.WebMercatorProjection(Cesium.Ellipsoid.WGS84)
+	});
