a01a10444e4b08ba88acc04ae22492a1416d3cdb
hikasan <wo__48@hotmail.com>
2016-05-10 15:02:41 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -20,12 +20,7 @@
         window.alert(error);
     });
 */
-	var testdata = './Data/test.geojson';
-
-	var promise = Cesium.GeoJsonDataSource.load(testdata);
-
-	promise.then(function(datasource){
-	  var viewer = new Cesium.Viewer('mapdiv', {
+  var viewer = new Cesium.Viewer('mapdiv', {
 //	    animation : false,
 //	    baseLayerPicker: false,
 //	    fullscreenButton: false,
@@ -35,35 +30,38 @@
 //	    sceneModePicker: false,
 //	    scene3DOnly: true,
 //	    timeline: false,
-        imageryProvider: new Cesium.OpenStreetMapImageryProvider({
-          url: 'http://cyberjapandata.gsi.go.jp/xyz/relief/'
-        }),
-        terrainProvider: new Cesium.JapanGSITerrainProvider({
-          heightPower: 1.0
-        })
-      });
+    imageryProvider: new Cesium.OpenStreetMapImageryProvider({
+      url: 'http://cyberjapandata.gsi.go.jp/xyz/relief/'
+    }),
+    terrainProvider: new Cesium.JapanGSITerrainProvider({
+      heightPower: 1.0
+    })
+  });
 
-      var layers = viewer.scene.imageryLayers;
-      var osm = layers.addImageryProvider(
-        new Cesium.OpenStreetMapImageryProvider()
-      );
-      osm.alpha = 0.6;
+  var layers = viewer.scene.imageryLayers;
+  var osm = layers.addImageryProvider(
+    new Cesium.OpenStreetMapImageryProvider()
+  );
+  osm.alpha = 0.6;
+  var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
+    imageryProvider: new Cesium.JapanGSIImageryProvider({
+        layerLists: ["ort","relief","std"]
+    }),
+    terrainProvider: new Cesium.JapanGSITerrainProvider({
+    }),
+    baseLayerPicker: false,
+    mapProjection: new Cesium.WebMercatorProjection(Cesium.Ellipsoid.WGS84)
+  });
 
-      var cesiumWidget = new Cesium.Viewer('cesiumContainer', {
-          imageryProvider: new Cesium.JapanGSIImageryProvider({
-              layerLists: ["ort","relief","std"]
-          }),
-          terrainProvider: new Cesium.JapanGSITerrainProvider({
-          }),
-          baseLayerPicker: false,
-          mapProjection: new Cesium.WebMercatorProjection(Cesium.Ellipsoid.WGS84)
-      });
-      var scene = cesiumWidget.scene;
-      //scene.globe.depthTestAgainstTerrain = true;
+  var testdata = './Data/test.geojson';
+  var promise = Cesium.GeoJsonDataSource.load(testdata);
+  promise.then(function(datasource){
+    var scene = cesiumWidget.scene;
+    //scene.globe.depthTestAgainstTerrain = true;
 
-      cesiumWidget.dataSources.add(datasource);
-      cesiumWidget.zoomTo(datasource);
-    }).otherwise(function(error){
-        window.alert(error);
-    });
+    cesiumWidget.dataSources.add(datasource);
+    cesiumWidget.zoomTo(datasource);
+  }).otherwise(function(error){
+    window.alert(error);
+  });
 
