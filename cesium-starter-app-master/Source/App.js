a4b5bbd4393007552e3941c5ddb97239c2fab4d3
hikasan <wo__48@hotmail.com>
2016-05-11 15:32:17 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -40,6 +40,7 @@ Cesium.Math.setRandomNumberSeed(0);
 //	        entity.cylinder.outlineColor = Cesium.Color.BLACK;
 //        }
         if (entity.billboard != null) {
+/*
             entities.add({
                 position : Cesium.Cartesian3.fromDegrees(entity.position.x, entity.position.y, 1000),
                 point : {
@@ -50,11 +51,12 @@ Cesium.Math.setRandomNumberSeed(0);
                     entity.point.outlineWidth = 3;
                 }
             });
+*/
             //var m = entity.remove();
             //entity.point.Color = color;
 //var wkCartesian3 = entity.position.clone();
             //entity.position = Cesium.Cartesian3.fromDegrees(wkCartesian3.x, wkCartesian3.y, 1000);
-            //entity.properties.TestName = '999';
+            entity.properties.TestName = '999';
             //entity.point.pixelSize = 8;
             //entity.point.color = Cesium.Color.TRANSPARENT;
             //entity.point.outlineColor = Cesium.Color.YELLOW;
