200eca9bba54e75bb10d6a55ad6f2df432f4c831
hikasan <wo__48@hotmail.com>
2016-05-11 15:52:48 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -31,6 +31,7 @@ Cesium.Math.setRandomNumberSeed(0);
 	        entity.polygon.material = color;
 	        entity.polygon.outline = false;
 	        entity.polygon.extrudedHeight = entity.properties.AverageValue * 10000.0;
+	        entity.properties.CityName ='xxx';
         }
 //        if (entity.corridor != null) {
 //	        entity.corridor.outlineColor = Cesium.Color.BLUE;
