a2df70682570b8d4a11e98c19730486557368a75
hikasan <wo__48@hotmail.com>
2016-05-11 11:49:18 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -64,7 +64,7 @@ Cesium.Math.setRandomNumberSeed(0);
         position : Cesium.Cartesian3.fromDegrees(140.77237129211426, 37.97918331477581,1000),
         billboard : {
             image : './Images/Iplus.png'
-        }
+        });
 
     cesiumWidget.dataSources.add(datasource);
     cesiumWidget.zoomTo(datasource);
