ccf5f1e9eda8865885b1c16277a727e0ea65d1ca
hikasan <wo__48@hotmail.com>
2016-06-02 11:27:33 +0900

    test

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -51,14 +51,14 @@ Cesium.Math.setRandomNumberSeed(0);
         if (entity.polygon != null) {
 	        //entity.polygon.material = color;
 	        //entity.polygon.material = Cesium.Color.fromHsl((0.6 - ((entity.properties.AverageValue - MinValue) / (MaxValue - MinValue) * 0.5)), 1.0, 0.5);
-	        entity.polygon.material = Cesium.Color.fromHsl((0.7 - ((entity.properties.AverageValue - MinValue) / (MaxValue - MinValue) * 0.5)), 1.0, 0.5);
+	        entity.polygon.material = Cesium.Color.fromHsl((0.8 - ((entity.properties.AverageValue - MinValue) / (MaxValue - MinValue) * 0.5)), 1.0, 0.5);
 	        entity.polygon.outline = false;
 	        //entity.polygon.extrudedHeight = entity.properties.AverageValue * 10.0;
 	        entity.polygon.extrudedHeight = (entity.properties.AverageValue - MinValue) / (MaxValue - MinValue) * 1000.0;
         }
         if (entity.polyline != null) {
 	        entity.polyline.material = Cesium.Color.BLUE;
-	        //entity.corridor.extrudedHeight = entity.properties.AverageValue * 10000.0;
+	        //entity.corridor.extrudedHeight = entity.properties.AverageValue * 1000.0;
         }
         
     }
